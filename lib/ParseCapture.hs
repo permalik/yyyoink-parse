d34a5d202aa99a4cf1b80b18be2f0ffc4021b6b9
permalik <permalik@protonmail.com>
2025-04-23 19:49:51 -0500

    feat: impl error handling

--- lib/ParseCapture.hs
+++ lib/ParseCapture.hs
@@ -1,16 +1,24 @@
 module ParseCapture where
 
+import qualified Control.Exception as Exception
 import qualified System.Environment as Env
+import qualified System.IO.Error as IOError
 
 runParseCapture :: IO ()
 runParseCapture =
-  handleArgs
-    >>= \fnameOrError ->
-      case fnameOrError of
-        Left err ->
-          putStrLn $ "Error processing: " <> err
-        Right fname ->
-          readFile fname >>= putStrLn
+  Exception.catch
+    ( handleArgs
+        >>= \arg ->
+          case arg of
+            Left err ->
+              putStrLn $ "Error processing: " <> err
+            Right fname ->
+              readFile fname >>= putStrLn
+    )
+    handleErr
+  where
+    handleErr :: IOError -> IO ()
+    handleErr e = putStrLn "An error occurred: " >> print e
 
 handleArgs :: IO (Either String FilePath)
 handleArgs =
@@ -21,3 +29,8 @@ handleArgs =
         [fname] -> Right fname
         [] -> Left "Failed: Must input filename"
         _ -> Left "Failed: Cannot input multiple files"
+
+eitherToErr :: (Show a) => Either a b -> IO b
+eitherToErr (Right a) = return a
+eitherToErr (Left e) =
+  Exception.throwIO . IOError.userError $ show e
